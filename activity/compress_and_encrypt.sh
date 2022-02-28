f8a18dee1aac5d705480f988c111c634aeab7417
Ryan Tanaka <tanaka@isi.edu>
2022-02-28 12:40:36 -0800

    update

--- activity/compress_and_encrypt.sh
+++ activity/compress_and_encrypt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for f in "*.txt"
-do
-    gzip --verbose --best $f
-    gpg --batch -c --passphrase-file pass.txt $f
-done
+gzip --verbose --best *.txt
+gpg --batch -c --passphrase-file pass 1.txt.gz
+gpg --batch -c --passphrase-file pass 2.txt.gz
+gpg --batch -c --passphrase-file pass 3.txt.gz
+gpg --batch -c --passphrase-file pass 4.txt.gz
+gpg --batch -c --passphrase-file pass 5.txt.gz
+gpg --batch -c --passphrase-file pass 6.txt.gz
