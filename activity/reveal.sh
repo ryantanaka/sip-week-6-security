1f94a9c6dda029342aea5ff82beba51901f57c9c
Ryan Tanaka <tanaka@isi.edu>
2022-02-28 12:52:33 -0800

    update

--- activity/reveal.sh
+++ activity/reveal.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
-for f in $(find . -name "*.txt.gz.gpg");
+for f in $(find . -name "*.txt.gz.gpg" | sort);
 do
+    echo "################################################################"
     echo $f
     compressed_result=$(echo $f | sed "s/\.gpg//") 
     final_result=$(echo $f | sed "s/\.gz\.gpg//")
     sha256sum $f;
-    gpg --batch --decrypt --passphrase-file pass.txt --output $compressed_result $f;
+    gpg --batch --decrypt --passphrase-file pass --output $compressed_result $f;
+    ls -alh $compressed_result
     gzip --verbose --decompress $compressed_result;
+    ls -alh $final_result
     cat $final_result
 done
